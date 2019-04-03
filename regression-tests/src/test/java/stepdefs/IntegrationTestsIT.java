17719d979f7def686a6e94d24bdc11904e3b0512
shail06 <shailesh.wadhwa@gmail.com>
2019-04-03 19:36:36 +0100

    B02-CUCUMBER-TESTS: Added Integration and Unit tests

---
+++ regression-tests/src/test/java/stepdefs/IntegrationTestsIT.java
@@ -0,0 +1,19 @@
+package stepdefs;
+
+import com.shail.pages.GoogleHome;
+import cucumber.api.java.en.Given;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+/**
+ * This class contains Integration tests and is executed by Failsafe Plugin
+ */
+public class IntegrationTestsIT {
+
+
+    @Test
+    public void firstIntegrationTest() throws Exception {
+        System.out.println("\t\n\nRUNNING INTEGRATION TESTS\t\n\n");
+    }
+
+}
